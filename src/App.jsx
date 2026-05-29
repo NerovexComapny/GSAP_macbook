23806d0786aeb0d023c469401e9281ba6b8bbf8d
Hamza Amiri <hamza.amiricoding@gmail.com>
2026-05-30 00:36:16 +0200

    implement showcase and other components

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,17 @@
 import NavBar from "./components/navBar.jsx";
 import Hero from "./components/Hero.jsx";
+import ProductViewer from "./components/ProductViewer.jsx";
+import gsap from "gsap";
+import{ ScrollTrigger} from "gsap/all";
+import Showcase from "./components/showcase.jsx";
+gsap.registerPlugin(ScrollTrigger);
 const App = () => {
     return (
         <main>
             <NavBar />
             <Hero />
+            <ProductViewer />
+            <Showcase />
         </main>
     )
 }
